2ed23566ac3769dd89debeb4f97fd33facb49d53
Noah <noahchamo@gmail.com>
2025-06-11 17:26:16 -0300

    back-end commit

--- back-end/mail.py
+++ back-end/mail.py
@@ -1,11 +1,30 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    },
+    2: {
+        "name": "Bread",
+        "price": 2.99,
+        "brand": "brown"
+    }
+}
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(description="The ID of the item you'd like to view")):
+    return inventory[item_id]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/get-by-name")
+def get_item(name: Optional[str] = None ):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data": "Not Found"}
+
+
+        
