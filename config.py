62feb88e83d9d6e7cb050f1d1eb76faaf3f4e0ce
Noah <noahchamo@gmail.com>
2025-07-13 22:21:44 -0300

    updated DB

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-APP_PASSWORD = 'ykhj uzeu yztv pmfe'
-
-DATABASE_URL = "postgresql://losdelfondosql_user:PvDDXaQUDSmRWaa4yL3Fq2zC1BkmRtAn@dpg-d16b2kumcj7s73bv3peg-a.oregon-postgres.render.com/losdelfondosql"
+DATABASE_URL = "postgresql://losdelfondosq_user:2Ca6S0Bwous385nwX9F60Ftb5CKprwzm@dpg-d1q5ack9c44c73982rmg-a.oregon-postgres.render.com/losdelfondosq"
 
 SECRET_KEY = "c67d978e20f38a9a00db5e4e60de978d93e0e3031b18e6c248e928bd3b9fad5b"
 ALGORITHM = "HS256"
