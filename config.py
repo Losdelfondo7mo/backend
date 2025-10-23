85870ab2cbb1fb6ef733a03c64c1ae03659b5f33
Noah <noahchamo@gmail.com>
2025-10-23 15:55:35 -0300

    db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DATABASE_URL = "postgresql://losdelfondosq_user:2Ca6S0Bwous385nwX9F60Ftb5CKprwzm@dpg-d1q5ack9c44c73982rmg-a.oregon-postgres.render.com/losdelfondosq"
+DATABASE_URL = "postgresql://losdelfondosq_ovgy_user:qKWcrBh2D07pnjlnbKMVriUJdXjdF8Rd@dpg-d3t76nbuibrs73cl7hq0-a.oregon-postgres.render.com/losdelfondosq_ovgy"
 
 SECRET_KEY = "c67d978e20f38a9a00db5e4e60de978d93e0e3031b18e6c248e928bd3b9fad5b"
 ALGORITHM = "HS256"
